1b46a605fd5d234ec640f1d8e3483f14810465dd
Jacek Szambelanczyk <jacekszambelanczyk@gmail.com>
2026-08-20 23:50:34 +0200

    Adding tests for user app

--- task_manager/users/tests.py
+++ task_manager/users/tests.py
@@ -1,3 +1,73 @@
-from django.test import TestCase
+import pytest
+from rest_framework.test import APIClient
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your tests here.
+User = get_user_model()
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("data, expected_status, expected_users_in_db", (
+    # Correct data
+    ({"username": "user123", "password": "password123321"}, 201, 1),
+    # Data without password
+    ({"username": "user321"}, 400, 0),
+    # Data without username
+    ({"password": "PASORD"}, 400, 0))
+)
+def test_user_registration(api_client, data, expected_status, expected_users_in_db):
+    url = "/api/users/registration/"
+    response = api_client.post(url, data, format="json")
+
+    assert response.status_code == expected_status
+    assert User.objects.count() == expected_users_in_db
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("data, expected_status", (
+    # Correct data
+    ({"username": "John321", "password": "password!321"}, 200),
+    # User doesn't exist
+    ({"username": "Apple34", "password": "Banana"}, 401),
+    # Wrong password
+    ({"username": "John321", "password": "password?123"}, 401))
+)
+def test_user_login(api_client, data, expected_status):
+    User.objects.create_user(username="John321", password="password!321")
+    url = "/api/users/login/"
+    response = api_client.post(url, data, format="json")
+
+    assert response.status_code == expected_status
+
+    if response.status_code == 200:
+        assert "access" in response.data
+        assert "refresh" in response.data 
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("is_token_valid, expected_status", (
+    # Valid token
+    (True, 200),
+    # Expired token
+    (False, 401))
+)
+def test_token_refresh(api_client, is_token_valid, expected_status):
+    user = User.objects.create_user(username="John321", password="password!321")
+    url = "/api/users/token/refresh/"
+
+    if is_token_valid:
+        refresh_token = str(RefreshToken.for_user(user))
+        data = {"refresh": refresh_token}
+    else:
+        data = {"refresh": "fake_token"}
+
+    response = api_client.post(url, data, format="json")
+
+    assert response.status_code == expected_status
+
+    if response.status_code == 200:
+        assert "access" in response.data
--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -3,7 +3,7 @@ from .views import RegisterView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path("api/registration/", RegisterView.as_view(), name="registration"),
-    path("api/login/", TokenObtainPairView.as_view(), name="login"),
-    path("api/token/refresh", TokenRefreshView.as_view(), name="token_refresh")
+    path("registration/", RegisterView.as_view(), name="registration"),
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh")
 ]
