ed890bfdcac4b89618238800e378221d889f453b
Jacek Szambelanczyk <jacekszambelanczyk@gmail.com>
2026-09-20 16:54:50 +0200

    Adding OpenAPI views documentation

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -4,10 +4,17 @@ from rest_framework import status
 from django.shortcuts import get_object_or_404
 from django.db.models import Case, When, Value, IntegerField
 from rest_framework.pagination import PageNumberPagination
+from drf_spectacular.utils import extend_schema, OpenApiParameter
+from drf_spectacular.types import OpenApiTypes
 from .serializers import TaskSerializer, TaskListSerializer
 from .models import Task
 
 class TaskDetailView(APIView):
+    @extend_schema(
+        description="Partial task editing. Allows you to change selected fields without overwriting the entire object.",
+        request=TaskSerializer,
+        responses={200: TaskSerializer}
+    )
     def patch(self, request, pk):
         task = get_object_or_404(Task, id=pk, user=request.user)
         serializer = TaskSerializer(instance=task, data=request.data, partial=True)
@@ -19,12 +26,20 @@ class TaskDetailView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+    @extend_schema(
+        description="Permanently deletes the task from the database.",
+        responses={204: None}
+    )
     def delete(self, request, pk):
         task = get_object_or_404(Task, id=pk, user=request.user)
         task.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
+    @extend_schema(
+        description="Returns full details of the selected task based on its ID.",
+        responses={200: TaskSerializer}
+    )
     def get(self, request, pk):
         task = get_object_or_404(Task, id=pk, user=request.user)
         serializer = TaskSerializer(instance=task)
@@ -36,6 +51,16 @@ class StandardResultSetPagination(PageNumberPagination):
 
 
 class TaskListView(APIView):
+    @extend_schema(
+        description="Downloading and filtering the tasks list. Returns a" \
+        " paged list of tasks for the logged in user.",
+        parameters=[
+            OpenApiParameter(name="status", description="Filter by status", type=OpenApiTypes.STR, many=True),
+            OpenApiParameter(name="priority", description="Filter by priorities", type=OpenApiTypes.STR, many=True),
+            OpenApiParameter(name="page", description="Page number (pagination)", type=OpenApiTypes.INT)
+        ],
+        responses=TaskListSerializer(many=True)
+    )
     def get(self, request):
         queryset = Task.objects.filter(user=request.user)
 
@@ -65,6 +90,11 @@ class TaskListView(APIView):
         return paginator.get_paginated_response(serializer.data)
 
 
+    @extend_schema(
+        description="Create a new task.",
+        request=TaskSerializer,
+        responses={201: TaskSerializer}
+    )
     def post(self, request):
         serializer = TaskSerializer(data=request.data)
 
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -2,24 +2,24 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
+from drf_spectacular.utils import extend_schema
 from .serializers import UserSerializer
 
 class RegisterView(APIView):
     permission_classes = (AllowAny,)
     authentication_classes = ()
 
+    @extend_schema(
+        description="Creates a new user account in the system. This endpoint is public and does not require a JWT token.",
+        request=UserSerializer,
+        responses={201: UserSerializer}
+    )
     def post(self, request):
         user_data = request.data
         serializer = UserSerializer(data=user_data)
 
         if serializer.is_valid():
             serializer.save()
-            return Response(
-                {"status": "The account has been created"},
-                status=status.HTTP_201_CREATED
-            )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-        return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST
-        )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
